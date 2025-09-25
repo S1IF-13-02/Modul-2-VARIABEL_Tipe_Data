cad454e9106266124515a8107e28cc9d8e90b51b
Ssandi <faridherdiyanto007@gmail.com>
2025-09-25 13:02:10 +0700

    Create luasLingkaran.go

---
+++ Farid Herdiyanto Vitasandi/luasLingkaran/luasLingkaran.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	const phi = 3.14
+	var r float64
+
+	fmt.Print("Masukkan panjang jari-jari: ")
+	fmt.Scan(&r)
+
+	luas := phi * r * r
+	
+	fmt.Println("Luas lingkaran adalah: ", luas)
+
+}
